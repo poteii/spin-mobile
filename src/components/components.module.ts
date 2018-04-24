4d2004ce6e8acb240515e822be358d97b3eac442
poteii <poteii.pasel@icloud.com>
2018-04-24 09:11:14 +0700

    by supreeya.ch

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,16 +1,16 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { TimestampComponent } from './timestamp/timestamp';
+import { TimetableDayComponent } from './timetable-day/timetable-day';
 
 import { CommonModule } from '@angular/common';
-import { TimetableDayComponent } from './timetable-day/timetable-day';
 @NgModule({
 	declarations: [
-		TimestampComponent, TimetableDayComponent
+		TimestampComponent,
+		TimetableDayComponent
 	],
 	imports: [CommonModule],
 	exports: [
 		TimestampComponent,
-		TimetableDayComponent],
-	schemas: [CUSTOM_ELEMENTS_SCHEMA]
+		TimetableDayComponent]
 })
 export class ComponentsModule { }
--- src/pages/work-time/work-time.module.ts
+++ src/pages/work-time/work-time.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { WorkTimePage } from './work-time';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
-    WorkTimePage,
+    WorkTimePage
   ],
   imports: [
     IonicPageModule.forChild(WorkTimePage),
+    ComponentsModule
   ],
 })
-export class WorkTimePageModule {}
+export class WorkTimePageModule { }
