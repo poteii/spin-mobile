1debe1ce19df237d37945e9e6819f441b5b38821
unknown <jannarong.san@gmail.com>
2018-04-26 17:38:36 +0700

    biwswalker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { MSG } from '../config/properties';
-import { AuthenticationProvider } from '../providers/authentication/authentication';
-import { HomePage } from '../pages/home/home';
-import { WorkTimePage } from '../pages/work-time/work-time';
 import { LoginPage } from '../pages/login/login';
 
 @Component({
@@ -15,7 +11,7 @@ import { LoginPage } from '../pages/login/login';
 
 export class MyApp {
   rootPage = LoginPage;
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private auth: AuthenticationProvider) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
